dd77ddf0fb6e9fe2ac6cabd54159f22e80c8821d
david <inge.davidch@gmail.com>
2025-04-11 00:22:15 -0600

    Alta de los servicios

--- Prueba_Tecnica_CRUD_API/Program.cs
+++ Prueba_Tecnica_CRUD_API/Program.cs
@@ -1,5 +1,6 @@
 using Microsoft.EntityFrameworkCore;
 using Prueba_Tecnica_CRUD_API.Data;
+using Prueba_Tecnica_CRUD_API.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -9,6 +10,9 @@ builder.Services.AddDbContext<ApplicationDbContext>(options =>
 
 
 // Add services to the container.
+builder.Services.AddScoped<IAlumnoService, AlumnoService>();
+builder.Services.AddScoped<ICursoService, CursoService>();
+builder.Services.AddScoped<IProfesorService, ProfesorService>();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
